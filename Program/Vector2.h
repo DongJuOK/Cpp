7933da5c3ca1f3cfac91f7b9a27e45dbf43437b7
DongJuOK <kcary0907@gmail.com>
2025-09-19 19:47:09 +0900

    (2025.9.19) 캡슐화

---
+++ Program/Vector2.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Vector2
+{
+private:
+	int x;
+	int y;
+
+public:
+	Vector2(int x, int y);
+
+	Vector2 & operator + (const Vector2 & clone);
+	Vector2 & operator - (const Vector2 & clone);
+	Vector2 & operator * (const Vector2 & clone);
+};
+
